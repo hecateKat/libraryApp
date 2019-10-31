607ae310bfdbe5f7024b4329138d5d9981062714
Kat <kat.ochmanska@gmail.com>
2019-10-31 16:08:31 +0100

    Book: typo corrected

--- src/main/java/com/kat/libraryapp/entities/Book.java
+++ src/main/java/com/kat/libraryapp/entities/Book.java
@@ -19,15 +19,15 @@ public class Book {
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
-    public Book(String title, String lsbn, String publisher) {
+    public Book(String title, String isbn, String publisher) {
         this.title = title;
-        this.isbn = lsbn;
+        this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String lsbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
         this.title = title;
-        this.isbn = lsbn;
+        this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
     }
